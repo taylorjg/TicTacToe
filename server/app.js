fceff5afb9335a38847677688498174ab38b149e
Jonathan Taylor <jonathan.taylor@dsl.pipex.com>
2016-04-16 14:55:04 +0100

    Game is working now - but doesn't correctly detect a draw (needs fixing)

--- server/app.js
+++ server/app.js
@@ -20,39 +20,92 @@ app.listen(port, function () {
 function handleComputerMove(req, res, _) {
     
     var board = req.body.board;
-    console.log(board);
     
     var win = checkForWin(board);
     if (win) {
-        var responseData = {
-            board: board,
-            gameOver: true,
-            winningPlayer: win.winningPlayer,
-            winningLine: win.winningLine
-        }
-        sendJsonResponse(res, 200, responseData);
-        return;   
+        return sendJsonResponse(res, 200, win);
     }
-    
+
     board = makeRandomMove(board);
     
     win = checkForWin(board);
     if (win) {
-        var responseData = {
-            board: board,
-            gameOver: true,
-            winningPlayer: win.winningPlayer,
-            winningLine: win.winningLine
-        }
-        sendJsonResponse(res, 200, responseData);
+        return sendJsonResponse(res, 200, win);
     }
     else {
         var responseData = {
             board: board,
             gameOver: false
         };
-        sendJsonResponse(res, 200, responseData);   
+        return sendJsonResponse(res, 200, responseData);   
     }
+}
+
+function makeRandomMove(board) {
+    var unoccupiedIndices = [];
+    for (var i = 0; i < board.length; i++) {
+        var ch = board[i];
+        if (ch !== "X" && ch !== "O") {
+            unoccupiedIndices.push(i);
+        }
+    }
+    var randomUnoccupiedIndex = getRandomIntInclusive(0, unoccupiedIndices.length - 1);
+    var index = unoccupiedIndices[randomUnoccupiedIndex];
+    board = setCharAt(board, "O", index);
+    return board;
+}
+
+function setCharAt(s, ch, index) {
+    var chs = [];
+    for (var i = 0; i < s.length; i++) {
+        chs.push(s[i]);
+    }
+    chs[index] = ch;
+    return chs.join("");
+}
+
+function getRandomIntInclusive(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function checkForWin(board) {
+    
+    var lines = [
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ];
+    
+    for (var i = 0; i < lines.length; i++) {
+        var line = lines[i];
+        var winningPlayer = checkForWinningLine(board, line);
+        if (winningPlayer) {
+            return {
+                board: board,
+                gameOver: true,
+                winningPlayer: winningPlayer,
+                winningLine: line
+            }
+        }
+    }
+    
+    return null;
+}
+
+function checkForWinningLine(board, line) {
+    var chs = "";
+    for (var i = 0; i < line.length; i++) {
+        var boardIndex = line[i];
+        chs += board[boardIndex];
+    }
+    if (chs === "XXX") return 1;
+    if (chs === "OOO") return 2;
+    return null;
 }
 
 function sendJsonResponse(res, status, content) {
@@ -64,44 +117,4 @@ function sendJsonResponse(res, status, content) {
         res.sendStatus(status);
         res.end();
     }
-}
-
-function makeRandomMove(board) {
-    // TODO: Make random move
-    return board;
-}
-
-function checkForWin(s) {
-    var lines = [
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ];
-    for (var i = 0; i < lines.length; i++) {
-        var line = lines[i];
-        var winningPlayer = checkLineForWin(s, line);
-        if (winningPlayer) {
-            return {
-                winningPlayer: winningPlayer,
-                winningLine: line
-            }
-        }
-    }
-    return null;
-}
-
-function checkLineForWin(s, line) {
-    var chs = "";
-    for (var i = 0; i < line.length; i++) {
-        var idx = line[i];
-        chs += s[idx];
-    }
-    if (chs === "XXX") return 1;
-    if (chs === "OOO") return 2;
-    return null;
 }
